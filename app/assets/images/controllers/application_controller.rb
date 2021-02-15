12251677600122d3ca6df306563df856336c600a
Will Theard <lavardwilltheard@yahoo.com>
2021-02-15 09:21:24 -0300

    set meta tags

--- app/assets/images/controllers/application_controller.rb
+++ app/assets/images/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 
   def set_locale
     I18n.locale = params.fetch(:locale, I18n.default_locale).to_sym
